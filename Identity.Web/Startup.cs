a3daf94bd4f05465ac216fb006d0cd871319f4d1
Berkan Çelik <79005929+Berkancelik@users.noreply.github.com>
2022-06-03 20:56:22 +0300

    Database connection strings

--- Identity.Web/Startup.cs
+++ Identity.Web/Startup.cs
@@ -1,6 +1,9 @@
+using Identity.Web.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -13,6 +16,8 @@ namespace Identity.Web
 {
     public class Startup
     {
+        public IConfiguration configuration { get; }
+
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
@@ -20,9 +25,13 @@ namespace Identity.Web
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<AppIdentityDbContext>(opts =>
+            {
+                opts.UseSqlServer(configuration["ConnectionStrings:DefaultConnectionString"]);
+            });
+            services.AddIdentity<AppUser,IdentityRole>().AddEntityFrameworkStores<AppIdentityDbContext>();
             services.AddControllersWithViews();
         }
 
