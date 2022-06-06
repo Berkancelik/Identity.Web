2940495c39a75a34d9250dd2a2452ec88cc4b6f8
Berkan Çelik <79005929+Berkancelik@users.noreply.github.com>
2022-06-06 07:37:55 +0300

    Designing the member homepage

--- Identity.Web/Controllers/MembersController.cs
+++ Identity.Web/Controllers/MembersController.cs
@@ -1,14 +1,32 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using Identity.Web.Models;
+using Identity.Web.ViewModels;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Mapster;
 
 namespace Identity.Web.Controllers
 {
+
+
     [Authorize]
     public class MembersController : Controller
     {
+        public UserManager<AppUser> userManager { get; }
+        public SignInManager<AppUser> signInManager { get; }
+
+        public MembersController(UserManager<AppUser> userManager, SignInManager<AppUser> signInManager)
+        {
+            this.userManager = userManager;
+            this.signInManager = signInManager;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            AppUser user = userManager.FindByNameAsync(User.Identity.Name).Result;
+            UserViewModel userViewModel = user.Adapt<UserViewModel>();
+
+            return View(userViewModel);
         }
     }
 }
