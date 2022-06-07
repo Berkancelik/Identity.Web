7eaba710f1b923f3b2bc8b880f3898a11c68c4a5
Berkan Çelik <79005929+Berkancelik@users.noreply.github.com>
2022-06-07 10:33:45 +0300

    Roles Deleted Operation has been completed

--- Identity.Web/Controllers/AdminController.cs
+++ Identity.Web/Controllers/AdminController.cs
@@ -1,57 +1,81 @@
 ﻿using Identity.Web.Models;
 using Identity.Web.ViewModels;
+using Mapster;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using System.Linq;
 
 namespace Identity.Web.Controllers
 {
-   
 
-        public class AdminController : BaseController
+
+    public class AdminController : BaseController
+    {
+        public AdminController(UserManager<AppUser> userManager, RoleManager<AppRole> roleManager) : base(userManager, null, roleManager)
         {
-            public AdminController(UserManager<AppUser> userManager, RoleManager<AppRole> roleManager) : base(userManager, null, roleManager)
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult RoleCreate()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult RoleCreate(RoleViewModel roleViewModel)
+        {
+            AppRole role = new AppRole();
+            role.Name = roleViewModel.Name;
+            IdentityResult result = roleManager.CreateAsync(role).Result;
+
+            if (result.Succeeded)
+
             {
+                return RedirectToAction("Roles");
+            }
+            else
+            {
+                AddModelError(result);
             }
 
-            public IActionResult Index()
+            return View(roleViewModel);
+        }
+
+        public IActionResult Roles()
+        {
+            return View(roleManager.Roles.ToList());
+        }
+
+        public IActionResult Users()
+        {
+            return View(userManager.Users.ToList());
+        }
+
+        public IActionResult RoleDelete(string id)
+        {
+            AppRole role = roleManager.FindByIdAsync(id).Result;
+            if (role != null)
             {
-                return View();
+                IdentityResult result = roleManager.DeleteAsync(role).Result;
             }
 
-            public IActionResult RoleCreate()
+            return RedirectToAction("Roles");
+        }
+
+        public IActionResult RoleUpdate(string id)
+        {
+            AppRole role = roleManager.FindByIdAsync(id).Result;
+
+            if (role != null)
             {
-                return View();
+                return View(role.Adapt<RoleViewModel>());
             }
 
-            [HttpPost]
-            public IActionResult RoleCreate(RoleViewModel roleViewModel)
-            {
-                AppRole role = new AppRole();
-                role.Name = roleViewModel.Name;
-                IdentityResult result = roleManager.CreateAsync(role).Result;
-
-                if (result.Succeeded)
-
-                {
-                    return RedirectToAction("Roles");
-                }
-                else
-                {
-                    AddModelError(result);
-                }
-
-                return View(roleViewModel);
-            }
-
-            public IActionResult Roles()
-            {
-                return View(roleManager.Roles.ToList());
-            }
-
-            public IActionResult Users()
-            {
-                return View(userManager.Users.ToList());
-            }
+            return RedirectToAction("Roles");
         }
     }
+}
