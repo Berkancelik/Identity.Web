de540f4f90b3586c92c1be8a0eb644f20b33c9fb
Berkan Çelik <79005929+Berkancelik@users.noreply.github.com>
2022-06-08 09:07:20 +0300

    Role assignment was successful

--- Identity.Web/Controllers/AdminController.cs
+++ Identity.Web/Controllers/AdminController.cs
@@ -4,6 +4,8 @@ using Mapster;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using System.Linq;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace Identity.Web.Controllers
 {
@@ -76,6 +78,86 @@ namespace Identity.Web.Controllers
             }
 
             return RedirectToAction("Roles");
+        }
+
+        [HttpPost]
+        public IActionResult RoleUpdate(RoleViewModel roleViewModel)
+        {
+            AppRole role = roleManager.FindByIdAsync(roleViewModel.Id).Result;
+
+            if (role != null)
+            {
+                role.Name = roleViewModel.Name;
+                IdentityResult result = roleManager.UpdateAsync(role).Result;
+
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Roles");
+                }
+                else
+                {
+                    AddModelError(result);
+                }
+            }
+            else
+            {
+                ModelState.AddModelError("", "Güncelleme işlemi başarısız oldu.");
+            }
+
+            return View(roleViewModel);
+        }
+
+        public IActionResult RoleAssign(string id)
+        {
+            TempData["userId"] = id;
+            AppUser user = userManager.FindByIdAsync(id).Result;
+
+            ViewBag.userName = user.UserName;
+
+            IQueryable<AppRole> roles = roleManager.Roles;
+
+            List<string> userroles = userManager.GetRolesAsync(user).Result as List<string>;
+
+            List<RoleAssignViewModel> roleAssignViewModels = new List<RoleAssignViewModel>();
+
+            foreach (var role in roles)
+            {
+                RoleAssignViewModel r = new RoleAssignViewModel();
+                r.RoleId = role.Id;
+                r.RoleName = role.Name;
+                if (userroles.Contains(role.Name))
+                {
+                    r.Exist = true;
+                }
+                else
+                {
+                    r.Exist = false;
+                }
+                roleAssignViewModels.Add(r);
+            }
+
+            return View(roleAssignViewModels);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> RoleAssign(List<RoleAssignViewModel> roleAssignViewModels)
+        {
+            AppUser user = userManager.FindByIdAsync(TempData["userId"].ToString()).Result;
+
+            foreach (var item in roleAssignViewModels)
+            {
+                if (item.Exist)
+
+                {
+                    await userManager.AddToRoleAsync(user, item.RoleName);
+                }
+                else
+                {
+                    await userManager.RemoveFromRoleAsync(user, item.RoleName);
+                }
+            }
+
+            return RedirectToAction("Users");
         }
     }
 }
---
+++ Identity.Web/ViewModels/RoleAssignViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Identity.Web.ViewModels
+{
+    public class RoleAssignViewModel
+    {
+        public string RoleId { get; set; }
+        public string RoleName { get; set; }
+        public bool Exist { get; set; }
+    }
+}
