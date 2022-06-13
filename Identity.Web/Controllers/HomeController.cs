d90b8f6e88c0ede9a6681420a4023df50bd9c2cc
Berkan Çelik <79005929+Berkancelik@users.noreply.github.com>
2022-06-13 09:48:04 +0300

    Email sender refactoring

--- Identity.Web/Controllers/HomeController.cs
+++ Identity.Web/Controllers/HomeController.cs
@@ -137,7 +137,7 @@ namespace Identity.Web.Controllers
 
                 //  www.bıdıbıdı.com/Home/ResetPasswordConfirm?userId=sdjfsjf&token=dfjkdjfdjf
 
-                Helper.PasswordReset.PasswordResetSendEmail(passwordResetLink);
+                Helper.PasswordReset.PasswordResetSendEmail(passwordResetLink,user.Email);
 
                 ViewBag.status = "success";
             }
--- Identity.Web/Helper/PasswordReset.cs
+++ Identity.Web/Helper/PasswordReset.cs
@@ -3,12 +3,12 @@ namespace Identity.Web.Helper
 {
     public class PasswordReset
     {
-        public static void PasswordResetSendEmail(string link)
+        public static void PasswordResetSendEmail(string link, string email)
         {
             MailMessage mail = new MailMessage();
             SmtpClient smtpClient = new SmtpClient("mail.example.com");
             mail.From = new MailAddress("berkancelikist@gmail.com");
-            mail.To.Add("celikberkan1@outlook.com");
+            mail.To.Add(email);
             mail.Subject = $"www.bıdı.com: Şifre sıfırlama";
             mail.Body = "<h2> Şifrenizi yenilemek için lütfen aşağıdkai linke tıklayımız</h2><hr/>";
             mail.Body+=$"<a href='{link}'>şifre yenileme linki</a>";
