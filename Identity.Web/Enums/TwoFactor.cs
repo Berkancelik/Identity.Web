f5b3c770032ac3faa1d67c6e70323d91752dcd25
Berkan Çelik <79005929+Berkancelik@users.noreply.github.com>
2023-02-20 23:34:20 +0300

    Enum created

---
+++ Identity.Web/Enums/TwoFactor.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Identity.Web.Enums
+{
+    public enum TwoFactor
+    {
+        [Display(Name = "Hiç Biri")]
+        None = 0,
+
+        [Display(Name = "Telefom ile kimlik doğrulama")]
+        Phone = 1, 
+
+        [Display(Name = "Email ile kimlik doğrulama")]
+        Email = 2, 
+
+        [Display(Name = "Microsoft-Google Authenticator ile kimlik doğrulama")]
+        MicrosoftGoogle = 3,
+
+    }
+}
